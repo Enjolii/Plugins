284f53d709e4194ec98de2285c9451d0dec0e129
Enjoli <Enjoli.van.mal@hva.nl>
2025-06-12 16:25:14 +0200

    :sparkles: Add all translations and working plugin thumbnail

--- frontend/projects/valtimo-plugins/open-product/src/lib/open-product-plugin.specification.ts
+++ frontend/projects/valtimo-plugins/open-product/src/lib/open-product-plugin.specification.ts
@@ -41,31 +41,59 @@ const openProductPluginSpecification: PluginSpecification = {
     pluginTranslations: {
         nl: {
             title: 'Open Product Plugin',
-            description:
-                '',
+            description: 'Deze plugin maakt het mogelijk om producten van de open producten API te beheren binnen Valtimo.',
             configurationTitle: 'Configuratienaam',
-            configurationTitleTooltip:
-                '',
+            configurationTitleTooltip: 'Configuratie naam is de naam die gebruikt wordt om de configuratie te identificeren.',
             baseUrl: 'URL',
+            productUUID: 'De UUID van het product',
+            productNaam: 'De naam van het product',
+            productTypeUUID: 'De UUID van het producttype',
+            eigenaarBSN: 'De bsn van de eigenaar',
+            eigenaarData: 'De gegevens van de eigenaar',
+            gepubliceerd: 'Kan het object getoond worden?',
+            productPrijs: 'De prijs van het product',
+            frequentie: 'De frequentie van de betalingen',
+            status: 'De status van het product',
+            resultaatPV: 'De naam van de process variable waar het resultaat in komt',
+            authenticationPluginConfiguration: 'Selecteer de authenticatie plugin configuratie',
         },
         en: {
             title: 'Open Product Plugin',
-            description: '',
-            configurationTitle: 'Configuration name',
-            configurationTitleTooltip:
-                '',
-            baseUrl: 'URL'
+            description: 'This plugin enables management of products from the Open Products API within Valtimo.',
+            configurationTitle: 'Configuration Name',
+            configurationTitleTooltip: 'The name used to identify this configuration.',
+            baseUrl: 'URL',
+            productUUID: 'The UUID of the product',
+            productNaam: 'The name of the product',
+            productTypeUUID: 'The UUID of the product type',
+            eigenaarBSN: 'The citizen service number (BSN) of the owner',
+            eigenaarData: 'The owner’s data',
+            gepubliceerd: 'Should this product be publicly visible?',
+            productPrijs: 'The price of the product',
+            frequentie: 'The payment frequency',
+            status: 'The product’s status',
+            resultaatPV: 'The name of the process variable that will contain the result',
+            authenticationPluginConfiguration: 'Select the authentication plugin configuration',
         },
         de: {
             title: 'Open Product Plugin',
-            description:
-                '',
-            configurationTitle: '',
-            configurationTitleTooltip:
-                '',
-            baseUrl: 'URL'
-        },
-    },
+            description: 'Dieses Plugin ermöglicht die Verwaltung von Produkten der Open Products API innerhalb von Valtimo.',
+            configurationTitle: 'Konfigurationsname',
+            configurationTitleTooltip: 'Der Name, mit dem diese Konfiguration identifiziert wird.',
+            baseUrl: 'URL',
+            productUUID: 'Die UUID des Produkts',
+            productNaam: 'Der Name des Produkts',
+            productTypeUUID: 'Die UUID des Produkttyps',
+            eigenaarBSN: 'Die BSN (Bürgernummer) des Eigentümers',
+            eigenaarData: 'Die Daten des Eigentümers',
+            gepubliceerd: 'Soll dieses Produkt öffentlich angezeigt werden?',
+            productPrijs: 'Der Preis des Produkts',
+            frequentie: 'Die Häufigkeit der Zahlungen',
+            status: 'Der Status des Produkts',
+            resultaatPV: 'Der Name der Prozessvariablen, in der das Ergebnis gespeichert wird',
+            authenticationPluginConfiguration: 'Wählen Sie die Authentifizierungs-Plugin-Konfiguration aus',
+        }
+    }
 };
 
 export {openProductPluginSpecification};
